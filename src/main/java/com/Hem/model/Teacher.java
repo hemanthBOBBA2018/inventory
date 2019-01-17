a1643313bdf101029457f4fc94b775fddb69e06a
hemanthBOBBA2018 <43779919+hemanthBOBBA2018@users.noreply.github.com>
2019-01-17 15:33:08 +0530

    Add files via upload

---
+++ src/main/java/com/Hem/model/Teacher.java
@@ -0,0 +1,46 @@
+package com.Hem.model;
+
+public class Teacher {
+       public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public Character getGender() {
+		return gender;
+	}
+	public void setGender(Character gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	private String id;
+       private String fullname;
+       private String qualification;
+       private Character gender;
+       private String address;
+       private String mobileNumber;
+}
